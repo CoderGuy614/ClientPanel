94aa039aaedb74299f2fbf12b40d0c85b84d2b1a
CoderGuy614 <jlutz.110@gmail.com>
2020-12-12 15:22:36 -0500

    Adding the Settings Page

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -8,12 +8,20 @@ export class SettingsService {
   settings: Settings = {
     allowRegistration: true,
     disableBalanceOnAdd: true,
-    disableBalanceOnEdit: false,
+    disableBalanceOnEdit: true,
   };
 
-  constructor() {}
+  constructor() {
+    if (localStorage.getItem('settings') !== null) {
+      this.settings = JSON.parse(localStorage.getItem('settings'));
+    }
+  }
 
   getSettings(): Settings {
     return this.settings;
+  }
+
+  changeSettings(settings: Settings) {
+    localStorage.setItem('settings', JSON.stringify(settings));
   }
 }
